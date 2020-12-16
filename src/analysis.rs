21aaacd534ea4dfdafd0ceb887b75df23a1273db
Will Greenberg <ifnspifn@gmail.com>
2020-12-16 02:27:30 -0800

    refactor freq analysis

--- src/analysis.rs
+++ src/analysis.rs
@@ -7,7 +7,7 @@ struct FrequencyComponent {
     coeff: f64,
 }
 
-fn get_primary_frequencies<T>(mut samples: Vec<T>, sample_rate: u32) -> Vec<FrequencyComponent> where T: Into<f64> {
+fn get_primary_frequencies<T>(mut samples: Vec<T>, sample_rate: u32, threshold: f64) -> Vec<FrequencyComponent> where T: Into<f64> {
     let n = samples.len();
     let bin_size = (sample_rate as f64) / (n as f64);
     let f64_samples: Vec<f64> = samples.drain(..).map(|s| s.into()).collect();
@@ -15,19 +15,17 @@ fn get_primary_frequencies<T>(mut samples: Vec<T>, sample_rate: u32) -> Vec<Freq
         .map(|s| s.mag() / (n as f64))
         .take(n/2)
         .collect();
-    let mut peaks = Vec::new();
-    for k in 1..n/2 - 1 {
-        if freq_domain[k] < 50.0 {
-            continue;
+    freq_domain.windows(3).enumerate().filter_map(|(k_prev, window)| {
+        let (prev, curr, next) = (window[0], window[1], window[2]);
+        if curr > threshold && curr > prev && curr > next {
+            Some(FrequencyComponent {
+                f: (k_prev + 1) as f64 * bin_size,
+                coeff: curr,
+            })
+        } else {
+            None
         }
-        if freq_domain[k-1] < freq_domain[k] && freq_domain[k] > freq_domain[k+1] {
-            peaks.push(FrequencyComponent {
-                f: (k as f64) * bin_size,
-                coeff: freq_domain[k],
-            });
-        }
-    }
-    peaks
+    }).collect()
 }
 
 #[cfg(test)]
@@ -41,7 +39,7 @@ mod tests {
         let mut reader = hound::WavReader::open("sine.wav").unwrap();
         let samples: Vec<i16> = reader.samples::<i16>().flat_map(|s| s).take(8192).collect();
         let sample_rate = reader.spec().sample_rate;
-        let freqs = get_primary_frequencies(samples, sample_rate);
+        let freqs = get_primary_frequencies(samples, sample_rate, 20.0);
         assert!(freqs.len() == 1);
         assert!(freqs[0].f - 440.0 < 10.0);
     }
